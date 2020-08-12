860ff11d56694e0040de00c6cfbf6f3ccc067830
Leon <leonlit123@gmail.com>
2020-08-12 17:10:06 +0800

    Redesigned the interface, now with better style and feel

--- lib/main.dart
+++ lib/main.dart
@@ -33,10 +33,17 @@ class Homepage extends StatefulWidget {
 }
 
 class _FetchDataState extends State<Homepage> {
-  final defaultFont = new TextStyle(
+  final fieldFont = new TextStyle(
     fontSize: 20,
     letterSpacing: 2.0,
     color: Colors.grey[600],
+  );
+
+  final btnFont = new TextStyle(
+    fontSize: 20.0,
+    letterSpacing: 2.0,
+    color: Colors.yellow[600],
+    fontWeight: FontWeight.bold,
   );
 
   final themeBg = Colors.yellow[600];
@@ -45,52 +52,44 @@ class _FetchDataState extends State<Homepage> {
   final userValue = TextEditingController(),
       convertedValue = TextEditingController();
 
-  String _fromvalue = "USD", _toValue = "USD";
+  String _fromValue = "USD", _toValue = "USD";
 
   static const List<String> CURRENCY = ["CAD", "HKD", "ISK", "USD"];
 
   void fetchData() async {
     final String amount = userValue.text;
-    print(amount);
-    print("to where : $_toValue");
-    print("from where : $_fromvalue");
 
     var response = await http.get(
         Uri.encodeFull(
-            "https://api.exchangeratesapi.io/latest?base=$_fromvalue&symbols=$_toValue"),
+            "https://api.exchangeratesapi.io/latest?base=$_fromValue&symbols=$_toValue"),
         headers: {"Accept": "application/json"});
 
     Map<String, dynamic> data = json.decode(response.body);
     final double rates = data["rates"][_toValue];
-    print("rates $rates");
     final double result = rates * double.parse(amount);
-    print(result);
+    convertedValue.text = result.toStringAsFixed(3);
   }
 
   @override
   Widget build(BuildContext context) {
-    return new Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        new Container(
-            height: 45,
-            width: 300.0,
-            alignment: Alignment.center,
-            child: new Row(children: <Widget>[
-              DropdownButtonHideUnderline(
-                  child: ButtonTheme(
-                      alignedDropdown: true,
+    return new Container(
+        margin: EdgeInsets.fromLTRB(20.0, 0, 20.0, 0),
+        child: new Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            new Container(
+                alignment: Alignment.center,
+                child: new Row(children: <Widget>[
+                  new Container(
+                      margin: EdgeInsets.fromLTRB(2.0, 0, 16.0, 0),
                       child: new DropdownButton(
-                          isExpanded: false,
-                          value: _fromvalue,
-                          icon: Icon(Icons.arrow_downward),
-                          iconSize: 24,
-                          elevation: 16,
-                          style: TextStyle(
-                              backgroundColor: themeBg, color: Colors.white),
+                          autofocus: true,
+                          focusColor: Colors.grey[300],
+                          value: _fromValue,
+                          style: btnFont,
                           onChanged: (String newValue) => {
                                 setState(() => {
-                                      _fromvalue = newValue,
+                                      _fromValue = newValue,
                                     })
                               },
                           items: [
@@ -99,42 +98,36 @@ class _FetchDataState extends State<Homepage> {
                                 value: i,
                                 child: Text('$i'),
                               )
-                          ]))),
-              Expanded(
-                  child: new TextField(
-                textAlign: TextAlign.center,
-                controller: userValue,
-                style: defaultFont,
-                decoration: InputDecoration(hintText: 'The amount to convert'),
-              ))
-            ])),
-        new Container(
-            margin: const EdgeInsets.all(20),
-            child: FlatButton(
-              color: themeBg,
-              onPressed: fetchData,
-              child: new Text(
-                "Convert",
-                style: defaultFont,
-              ),
-            )),
-        new Container(
-            height: 45,
-            width: 300.0,
-            alignment: Alignment.center,
-            decoration: BoxDecoration(border: Border.all(color: btnBg)),
-            child: new Row(children: <Widget>[
-              DropdownButtonHideUnderline(
-                  child: ButtonTheme(
-                      padding: const EdgeInsets.all(20),
-                      alignedDropdown: true,
+                          ])),
+                  Expanded(
+                      child: new TextField(
+                    textAlign: TextAlign.center,
+                    textAlignVertical: TextAlignVertical.center,
+                    controller: userValue,
+                    style: fieldFont,
+                    decoration: InputDecoration(
+                      hintText: 'Amount Value',
+                    ),
+                  ))
+                ])),
+            new Container(
+                margin: const EdgeInsets.all(20),
+                child: FlatButton(
+                  color: themeBg,
+                  onPressed: fetchData,
+                  child: new Text(
+                    "Convert",
+                    style: fieldFont,
+                  ),
+                )),
+            new Container(
+                alignment: Alignment.center,
+                child: new Row(children: <Widget>[
+                  new Container(
+                      margin: EdgeInsets.fromLTRB(2.0, 0, 16.0, 0),
                       child: new DropdownButton(
                           value: _toValue,
-                          icon: Icon(Icons.arrow_downward),
-                          iconSize: 24,
-                          elevation: 16,
-                          style: TextStyle(
-                              backgroundColor: themeBg, color: Colors.white),
+                          style: btnFont,
                           onChanged: (String newValue) => {
                                 setState(() => {
                                       _toValue = newValue,
@@ -146,17 +139,21 @@ class _FetchDataState extends State<Homepage> {
                                 value: i,
                                 child: Text('$i'),
                               )
-                          ]))),
-              Expanded(
-                  child: new TextField(
-                textAlign: TextAlign.center,
-                controller: convertedValue,
-                style: defaultFont,
-                enabled: false,
-              )),
-            ]))
-      ],
-    );
+                          ])),
+                  Expanded(
+                      child: new TextField(
+                    textAlign: TextAlign.center,
+                    textAlignVertical: TextAlignVertical.center,
+                    controller: convertedValue,
+                    style: fieldFont,
+                    enabled: false,
+                    decoration: InputDecoration(
+                      hintText: "Converted Value",
+                    ),
+                  )),
+                ]))
+          ],
+        ));
   }
 }
 
