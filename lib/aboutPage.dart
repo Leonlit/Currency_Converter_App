ea482bbad8f9df0bc7a004f0e4a965effbc348ff
Leon <leonlit123@gmail.com>
2020-08-17 22:04:12 +0800

    Preparing app for deployment

--- lib/aboutPage.dart
+++ lib/aboutPage.dart
@@ -41,7 +41,7 @@ class AboutPageWidget extends StatelessWidget {
                 left: 10.0, right: 10.0, top: 15.0, bottom: 40.0),
             child: Linkable(
               text:
-                  "This app is created so that user could convert an amount of money from a currency to another currency with ease. If you're interested in the source code, you could find the link in the navigation menu or simply press this link\n\nhttps://github.com/Leonlit/Currency_Converter_App\n\nFeel free to contact me if you have any suggestions, questions, or critics \n\nemail: \nleonlit123@gmail.com\n\nreddit: \nhttps://www.reddit.com/user/scrolion\n\ntwitter: \nhttps://twitter.com/leonlit",
+                  "This app is created so that user could convert an amount of money from one currency to another currency with ease. If you're interested in the source code, you could find the link in the navigation menu or simply press this link\n\nhttps://github.com/Leonlit/Currency_Converter_App\n\nFeel free to contact me if you have any suggestions, questions, or critics \n\nemail: \nleonlit123@gmail.com\n\nreddit: \nhttps://www.reddit.com/user/scrolion\n\ntwitter: \nhttps://twitter.com/leonlit",
               textAlign: TextAlign.left,
               style: AppTheme.pageFont,
             )),
